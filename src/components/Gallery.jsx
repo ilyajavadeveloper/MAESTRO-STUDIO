f3b403ad19f01cd5183a5fdc16a4e16b54f785cd
Ilya Hvostenko <ilyajavadev@gmail.com>
2025-05-20 16:51:18 +0300

    fix: update gallery imports paths and translations

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,13 @@
+// src/components/Gallery.jsx
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './Gallery.css';
 
-import img1 from '../assets/gallery-1.jpg';
-import img2 from '../assets/gallery-2.jpg';
-import img3 from '../assets/gallery-3.jpg';
-import img4 from '../assets/gallery-4.jpg';
-import img5 from '../assets/jpg5.png';
+import img1 from '../assets/gallery-1.png';
+import img2 from '../assets/gallery-2.png';
+import img3 from '../assets/gallery-3.png';
+import img4 from '../assets/gallery-4.png';
+import img5 from '../assets/gallery-5.png';
 
 const images = [img1, img2, img3, img4, img5];
 
@@ -16,10 +17,13 @@ const Gallery = () => {
   return (
     <section id="gallery" className="gallery">
       <h2>{t('gallery.title')}</h2>
-      <div className="carousel">
+      <div className="grid">
         {images.map((src, idx) => (
-          <div key={idx} className="carousel-item">
-            <img src={src} alt={`${t('gallery.altPrefix')} ${idx + 1}`} />
+          <div key={idx} className="grid-item">
+            <img
+              src={src}
+              alt={`${t('gallery.altPrefix')} ${idx + 1}`}
+            />
           </div>
         ))}
       </div>
